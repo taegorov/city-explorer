60087bd6fa8a41c41922e9a5610417a989a4df0c
taegorov <taegorov@gmail.com>
2021-04-19 15:53:35 -0700

    adds CSS styling, adds map functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import './index.css';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -19,6 +21,7 @@ class App extends React.Component {
   }
 
   render() {
+    const img_url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${this.state.location.lat},${this.state.location.lon}&size=${window.innerWidth}x300&format=jpg&zoom=12`;
     return (
       <>
         <input onChange={(e) => this.setState({ searchQuery: e.target.value })} placeholder="city name here"></input>
@@ -27,11 +30,13 @@ class App extends React.Component {
           <h2>The city is: {this.state.location.display_name}</h2>
         }
         {this.state.location.place_id &&
-          <h2>Latitude is: {this.state.location.lat}</h2>
+          <h3>Latitude is: {this.state.location.lat}</h3>
         }
         {this.state.location.place_id &&
-          <h2>Longitude is: {this.state.location.lon}</h2>
+          <h3>Longitude is: {this.state.location.lon}</h3>
         }
+        <br></br>
+        <img src={img_url} alt="location" id="map"/>
       </>
     )
   }
