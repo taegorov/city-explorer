074579fe9f8bbc73432f18ca5e58902219cde015
taegorov <taegorov@gmail.com>
2021-04-22 16:41:46 -0700

    refactors, creates WeatherDay and moves over info from Weather

---
+++ src/WeatherDay.js
@@ -0,0 +1,32 @@
+'use strict'
+
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+class WeatherDay extends React.Component {
+
+  render() {
+    return (
+      <ListGroup.Item key={this.props.index} as="li" action variant="dark"active>
+        Day: {this.props.day.time}
+        Forecast: {this.props.day.forecast}
+      </ListGroup.Item>
+    )
+  }
+}
+
+export default WeatherDay;
+
+
+
+// dont forget to import into parent component!
+
+
+
+// this.props.weather.map((day, index) => (
+
+//   <ListGroup.Item key={index} as="li" active>
+//     Day: {day.time + ' '}
+//     Forecast: {day.forecast}
+//   </ListGroup.Item>
